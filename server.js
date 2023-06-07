e9e8a74070e4c5018b889dfff29c6d077fa9e77e
NguyenTrongQuan <112452601+hapo-quannt@users.noreply.github.com>
2023-06-07 23:21:40 +0700

    next

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Định nghĩa route handler cho đường dẫn gốc
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Đây là web của Quân <br> Next :  access Database - demo crud');
 });
 
 app.listen(3000, () => {
