2733b7c5e4b953bceacf6cad394691aa268db167
NguyenTrongQuan <112452601+hapo-quannt@users.noreply.github.com>
2023-06-08 00:47:44 +0700

    next

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const mysql = require("mysql")
 
 // Định nghĩa route handler cho đường dẫn gốc
 app.get('/', (req, res) => {
-  res.send('Đây là web của Quân <br> Next :  access Database - demo crud');
+  const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  })
+  db.connect((err) => {
+    if(err) {
+      res.send(`Đây là web của Quân <br> Next :  access Database - demo crud <br> Database status : ${err.message}`);
+    }
+    res.send('Đây là web của Quân <br> Next :  access Database - demo crud <br> Database status : Success');
+  })
 });
 
 app.listen(3000, () => {
